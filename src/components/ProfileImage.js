16e3d90382e2f1305d92756ca64a6d0797418479
Urvi Pandit <95270207+Urvi-Pandit@users.noreply.github.com>
2022-10-22 09:28:24 +0530

    Update ProfileImage.js

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -3,7 +3,7 @@ const ProfileImage = () => {
     return (
         <div style={{}}>
             <img style={{borderRadius: 500/2, marginLeft: '50px', marginTop: '10px'}} 
-            src="https://raw.githubusercontent.com/Urvi-Pandit/IP-React/main/src/components/cropped.jpg" 
+            src="https://raw.githubusercontent.com/Urvi-Pandit/profile-page/master/src/components/cropped.jpg" 
             width="200" height="200" alt="Urvi"></img>
             <h1 style={{alignContent: 'center', marginLeft: '40px', fontSize: '30px', }}>Hi! I am Urvi Pandit</h1>
         </div>
